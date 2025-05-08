1dfb5cf9ecb7cc1085b9fd821fa9a8ed97267a8f
Rorisang <rorisang.shadung@bbd.co.za>
2025-05-08 21:07:33 +0200

    AddedAuthScreenComponent

--- balderdash-app/jsweb/public/app.js
+++ balderdash-app/jsweb/public/app.js
@@ -1,15 +1,22 @@
 import { router } from '../router/index.js';
 import { HomePage } from '../pages/home-page.js';
+import { AuthPage } from '../pages/auth.js';
 import { LoadingSpinner } from '../components/loading-spinner.js';
 
 // Register Web Components
 customElements.define('home-page', HomePage);
-
+customElements.define('auth-page', AuthPage);
 // Configure Routes
 router.addRoute('home', (data) => {
   const homePage = new HomePage();
   homePage.routeData = data;
   document.querySelector('#app').appendChild(homePage);
+});
+
+router.addRoute('auth', (data) => {
+  const authPage = new AuthPage();
+  authPage.routeData = data;
+  document.querySelector('#app').appendChild(authPage);
 });
 
 // Initialize router
