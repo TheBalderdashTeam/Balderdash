580f0427fe0b647462181edb92878fc1564f730d
Bernard Paetzold <bernard.paetzold@bbd.co.za>
2025-05-10 11:25:11 +0200

    Small fixes

--- balderdash-app/src/controllers/GameController.ts
+++ balderdash-app/src/controllers/GameController.ts
@@ -2,62 +2,82 @@ import { Request, Response } from 'express';
 import { GameService } from '../services/GameService';
 
 export class GameController {
-  static async createGame(req: Request, res: Response) {
-    try {
-      const { hostUserId, numberRounds, timeLimitSeconds, statusId } = req.body;
-      const game = await GameService.createGame(hostUserId, numberRounds, timeLimitSeconds, statusId);
-      res.status(200).json(game);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error creating game' });
+    static async createGame(req: Request, res: Response) {
+        try {
+            const { hostUserId, numberRounds, timeLimitSeconds, statusId } =
+                req.body;
+            const game = await GameService.createGame(
+                hostUserId,
+                numberRounds,
+                timeLimitSeconds,
+                statusId
+            );
+            res.status(200).json(game);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error creating game' });
+        }
     }
-  }
 
-  static async updateGameStatus(req: Request, res: Response): Promise<any> {
-    try {
-      const gameId = parseInt(req.params.id);
-      const { statusId } = req.body;
-      const game = await GameService.updateGameStatus(gameId, statusId);
-      if (!game) return res.status(404).json({ message: 'Game not found' });
-      res.status(200).json(game);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error updating game status' });
-    }
-  }
+    static async startGame(req: Request, res: Response): Promise<any> {
+        try {
+            const gameId = parseInt(req.params.id);
 
-  static async endGame(req: Request, res: Response): Promise<any> {
-    try {
-      const gameId = parseInt(req.params.id);
-      const game = await GameService.endGame(gameId);
-      if (!game) return res.status(404).json({ message: 'Game not found' });
-      res.status(200).json(game);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error ending game' });
+            GameService.startGame(gameId);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error starting game' });
+        }
     }
-  }
 
-  static async deleteGame(req: Request, res: Response): Promise<any> {
-    try {
-      const gameId = parseInt(req.params.id);
-      await GameService.deleteGame(gameId);
-      res.status(200).send();
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error deleting game' });
+    static async updateGameStatus(req: Request, res: Response): Promise<any> {
+        try {
+            const gameId = parseInt(req.params.id);
+            const { statusId } = req.body;
+            const game = await GameService.updateGameStatus(gameId, statusId);
+            if (!game)
+                return res.status(404).json({ message: 'Game not found' });
+            res.status(200).json(game);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error updating game status' });
+        }
     }
-  }
 
-  static async getGameById(req: Request, res: Response): Promise<any> {
-    try {
-      const gameId = parseInt(req.params.id);
-      const game = await GameService.getGameById(gameId);
-      if (!game) return res.status(404).json({ message: 'Game not found' });
-      res.status(200).json(game);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error fetching game' });
+    static async endGame(req: Request, res: Response): Promise<any> {
+        try {
+            const gameId = parseInt(req.params.id);
+            const game = await GameService.endGame(gameId);
+            if (!game)
+                return res.status(404).json({ message: 'Game not found' });
+            res.status(200).json(game);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error ending game' });
+        }
     }
-  }
+
+    static async deleteGame(req: Request, res: Response): Promise<any> {
+        try {
+            const gameId = parseInt(req.params.id);
+            await GameService.deleteGame(gameId);
+            res.status(200).send();
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error deleting game' });
+        }
+    }
+
+    static async getGameById(req: Request, res: Response): Promise<any> {
+        try {
+            const gameId = parseInt(req.params.id);
+            const game = await GameService.getGameById(gameId);
+            if (!game)
+                return res.status(404).json({ message: 'Game not found' });
+            res.status(200).json(game);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error fetching game' });
+        }
+    }
 }
---
+++ balderdash-app/src/types/GameState.ts
@@ -0,0 +1,6 @@
+export enum GameState {
+    None,
+    Pending,
+    Active,
+    Complete,
+}
